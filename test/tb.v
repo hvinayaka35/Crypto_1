21a5e287412965b32575bebda6c751a18f9720c8
hvinayaka35 <hvinayaka35@gmail.com>
2026-08-20 12:12:52 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_vinayaka_ntt_bfly with your module name:
-  tt_um_vinayaka_ntt_bfly user_project (
+  tt_um_pqc_ntt_bfly user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
